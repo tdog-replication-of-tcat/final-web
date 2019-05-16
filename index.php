ead569ed1d4d82179fdbb969b02102d5a3deec8c
Jasper Yu <007seadog@gmail.com>
2019-05-16 21:30:25 +0000

    Added commit for iframe wrapper

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     <head>
     </head>
     <body>
+        <!--believe or not, the original iframe set is much worse-->
 	<iframe src="main.php" style="width: 100%; height: 100%; border: none;"></iframe>
     </body>
 </html>
