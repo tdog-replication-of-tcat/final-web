7355e5d8275343772bbdcfc3c631b66ee0caac34
Jasper Yu <007seadog@gmail.com>
2019-05-16 13:57:56 +0000

    Added index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <center>
+	    <img src="banner.jpg">
+            <h1>T狗盃<del>資安</del>競賽</h1>
+	    <hr>
+            <p>注意事項blablabla<p>
+            <form action="dashboard.php" method="post">
+	        <label>神馬隊伍代碼ㄉ：</label><input type="textbox" name="somecode"/><input type="submit">
+            </form>
+        </center>
+    </body>
+</html>
